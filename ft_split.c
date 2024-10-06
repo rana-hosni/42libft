5604f257309c937642f4f00ce6d247d99178a689
Rana.home <ranahosni90@gmail.com>
2024-10-06 13:18:30 +0200

    temp

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: relgheit <relgheit@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rana <rana@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 10:22:24 by relgheit          #+#    #+#             */
-/*   Updated: 2024/10/04 14:39:56 by relgheit         ###   ########.fr       */
+/*   Updated: 2024/10/06 12:56:11 by rana             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,71 +17,90 @@ static int	count_word(char *s, char c)
 	int	i;
 	int	count;
 
-	i = 0;
+	i = 1;
 	count = 0;
 	while (s[i])
 	{
-		if (s[0] == c)
-			i++;
-		if (ft_strchr(&s[i], c))
+		if (s[i] == c && s[i - 1] != c)
+			count++;
+		if (s[i + 1] == '\0' && s[i] != c)
 			count++;
 		i++;
 	}
 	return (count);
 }
 
-static char	*the_word(char *s, int start, int end)
+static char	*the_word(const char *s, char c)
 {
-	int		i;
 	char	*word;
+	int		i;
 
 	i = 0;
-	word = malloc(sizeof(char) * (end - start +1));
-	while (start < end)
-	{
-		word[i] = s[start];
+	while (s[i] != c && s[i] != '\0')
 		i++;
-		start++;
+	word = malloc(sizeof(char) * i + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+	{
+		word[i] = s[i];
+		i++;
 	}
 	word[i] = '\0';
 	return (word);
 }
-// static char	**free_all(char **arr, int i)
-// {
-// 	while (i >= 0)
-// 	{
-// 		free(arr[i]);
-// 		i--;
-// 	}
-// 	free(arr);
-// 	return (NULL);
-// }
+
+static char	**free_all(char **arr, int i)
+{
+	while (i >= 0)
+	{
+		free(arr[i]);
+		i--;
+	}
+	free(arr);
+	return (NULL);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
-	size_t	i;
+	int		i;
 	size_t	j;
-	size_t	flag;
 
 	i = 0;
 	j = 0;
-	flag = -1;
 	arr = malloc(sizeof(char *) * (count_word((char *)s, c) + 1));
 	if (!arr || !s)
 		return (NULL);
-	while (i < ft_strlen(s))
+	while (i < count_word((char *)s, c))
 	{
-		if (s[i] != c && flag < 0)
-			flag = i;
-		else if (((s[i] == c || i == ft_strlen(s)) && flag >= 0))
+		while (s[j] == c)
+			j++;
+		arr[i] = the_word(&s[j], c);
+		if (!arr[i])
 		{
-			arr[j] = the_word((char *)s, flag, i);
-			flag = -1;
+			free_all(arr, i - 1);
+			return (NULL);
 		}
-		j++;
 		i++;
+		while (s[j] != c && s[j] != '\0')
+			j++;
 	}
-	arr[j] = NULL;
+	arr[i] = NULL;
 	return (arr);
+}
+#include <stdio.h>
+int main(void)
+{
+	char **tabstr = ft_split("hello world", ' ');
+	int i = 0;
+	// printf("Calling count from main, count = %d\n", count_word("ahmed helmi", ' '));
+	while (tabstr[i] != NULL)
+			{
+				printf("%s", tabstr[i]);
+				write(1, "\n", 1);
+				i++;
+			}
+	return 0;
 }
